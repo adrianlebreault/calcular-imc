543fedbf2a333cde72275d70e84a226914248e92
Adrian <adrian.lebreault@gmail.com>
2024-07-03 21:31:28 -0400

    Hello

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function calcularIMC() {
     const imc = (peso / (altura * altura)).toFixed(2);
     let mensaje;
     if (imc < 18.5) {
-    mensaje = "Tu IMC es " + imc + ". Estás bajo de peso. Consulta con un nutricionista para una dieta adecuada.";
+        mensaje = "Tu IMC es " + imc + ". Estás bajo de peso. Consulta con un nutricionista para una dieta adecuada.";
     } else if (imc >= 18.5 && imc < 24.9) {
-    mensaje = "Tu IMC es " + imc + ". Tienes un peso normal. Mantén una dieta equilibrada.";
+        mensaje = "Tu IMC es " + imc + ". Tienes un peso normal. Mantén una dieta equilibrada.";
     } else if (imc >= 25 && imc < 29.9) {
-     mensaje = "Tu IMC es " + imc + ". Tienes sobrepeso. Considera una dieta balanceada y ejercicio regular.";
+        mensaje = "Tu IMC es " + imc + ". Tienes sobrepeso. Considera una dieta balanceada y ejercicio regular.";
     } else {
         mensaje = "Tu IMC es " + imc + ". Estás en obesidad. Consulta con un especialista para una dieta y plan de ejercicio.";
- }
+    }
 
     document.getElementById('resultado').innerText = `Tu IMC es ${imc}`;
 }
